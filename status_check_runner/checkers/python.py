afd63a0dfec36187d254ab06afc5b6f6977a8fae
William Sawyer <wmsawyer2609@gmail.com>
2024-07-29 12:51:54 +0000

    Combine Python paths into one command per check

--- status_check_runner/checkers/python.py
+++ status_check_runner/checkers/python.py
@@ -16,25 +16,26 @@ class PythonChecker(Checker):
     def get_checks(config: Config) -> list[Check]:
         checks: list[Check] = []
 
-        for path in config.paths:
-            checks.append(
-                Check(
-                    name=f"Ruff Linting ({path})",
-                    command=f"ruff check {path}",
-                )
+        paths = " ".join(map(str, config.paths))
+
+        checks.append(
+            Check(
+                name="Ruff Linting",
+                command=f"ruff check {paths}",
             )
-            checks.append(
-                Check(
-                    name=f"Ruff Formatting ({path})",
-                    command=f"ruff format --check {path}",
-                )
+        )
+        checks.append(
+            Check(
+                name="Ruff Formatting",
+                command=f"ruff format --check {paths}",
             )
-            checks.append(
-                Check(
-                    name=f"MyPy Type Checking ({path})",
-                    command=f"mypy {path}",
-                )
+        )
+        checks.append(
+            Check(
+                name="MyPy Type Checking",
+                command=f"mypy {paths}",
             )
+        )
 
         if config.tests_path:
             checks.append(
