b0bab437c321d83b80c8719c337bc39a54f51ab3
William Sawyer <wmsawyer2609@gmail.com>
2024-07-30 13:32:49 +0000

    Remove pytest args from Python checker config

--- status_check_runner/checkers/python.py
+++ status_check_runner/checkers/python.py
@@ -10,7 +10,6 @@ class PythonChecker(Checker):
     class Config(BaseModel):
         paths: list[Path] = []
         tests_path: Optional[Path] = None
-        pytest_args: list[str] = []
 
     @staticmethod
     def get_checks(config: Config) -> list[Check]:
@@ -23,11 +22,6 @@ class PythonChecker(Checker):
         checks.append(Check(name="MyPy Type Checking", command=f"mypy {paths}"))
 
         if config.tests_path:
-            checks.append(
-                Check(
-                    name=f"Pytest Tests ({config.tests_path})",
-                    command=f"pytest {" ".join(config.pytest_args)} {config.tests_path}",
-                )
-            )
+            checks.append(Check(name="Pytest Tests", command=f"pytest {config.tests_path}"))
 
         return checks
