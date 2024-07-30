51473b67cac41e999e332a4fb7c9bb69ea503834
William Sawyer <wmsawyer2609@gmail.com>
2024-07-30 03:29:40 +0000

    Include check in result

--- status_check_runner/execute.py
+++ status_check_runner/execute.py
@@ -7,6 +7,7 @@ from .checkers import Check
 
 
 class Result(BaseModel):
+    check: Check
     status: int
     stdout: str
     stderr: Optional[str] = None
@@ -15,6 +16,6 @@ class Result(BaseModel):
 def execute_check(check: Check) -> Result:
     try:
         output = subprocess.check_output(check.command.split(), stderr=subprocess.STDOUT, text=True)
-        return Result(status=0, stdout=output)
+        return Result(check=check, status=0, stdout=output)
     except subprocess.CalledProcessError as exc:
-        return Result(status=exc.returncode, stdout=exc.stdout, stderr=exc.stderr)
+        return Result(check=check, status=exc.returncode, stdout=exc.stdout, stderr=exc.stderr)
