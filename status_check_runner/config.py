2ab2b52fd820b3f79872736e647f9819f0cf0555
William Sawyer <wmsawyer2609@gmail.com>
2024-07-30 11:56:35 +0000

    Resolve JSON schema definition references

--- status_check_runner/config.py
+++ status_check_runner/config.py
@@ -1,6 +1,7 @@
 import json
-from typing import Optional
+from typing import Any, Optional
 
+import jsonref
 import tomllib
 
 from .checkers.default import DefaultChecker
@@ -23,5 +24,13 @@ def get_config() -> Config:
 
 
 def update_config_json_schema():
+    json_schema = Config.model_json_schema()
+    json_schema: dict[str, Any] = jsonref.replace_refs(json_schema)  # type: ignore
+
+    # Shallow copy so that we can remove `$defs` from the schema which is to be dumped.
+    # The original copy still has `$defs` and hence references can be resolved.
+    new_json_schema = json_schema.copy()
+    new_json_schema.pop("$defs")
+
     with open("config-schema.json", "w", encoding="utf-8") as file:
-        json.dump(Config.model_json_schema(), file, indent=4)
+        json.dump(new_json_schema, file, indent=4)
