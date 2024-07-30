a4f14d6e80e9928ed9a6fada3542650915cde57b
William Sawyer <wmsawyer2609@gmail.com>
2024-07-30 07:18:05 +0000

    Report failure in GitHub Actions

--- status_check_runner/reporters/github_actions.py
+++ status_check_runner/reporters/github_actions.py
@@ -1,3 +1,5 @@
+from typing import Iterable
+
 from actions_toolkit import core
 
 from ..execute import Result
@@ -5,10 +7,17 @@ from . import Reporter
 
 
 class GitHubActionsReporter(Reporter):
+    def __init__(self):
+        super().__init__()
+
+        self.failures: list[Result] = []
+
     def report_success(self, result: Result):
         core.debug(f"{result.check.name} succeeded!")
 
     def report_failure(self, result: Result):
+        self.failures.append(result)
+
         core.start_group(f"{result.check.name} failed ({result.status})")
 
         core.warning(result.stdout)
@@ -17,3 +26,9 @@ class GitHubActionsReporter(Reporter):
             core.error(result.stderr)
 
         core.end_group()
+
+    def report_all(self, results: Iterable[Result]):
+        super().report_all(results)
+
+        if self.failures:
+            core.set_failed(f"{len(self.failures)} checks failed")
