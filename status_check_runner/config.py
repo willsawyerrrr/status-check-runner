9c63588315d2cd31db68776b1fc4c6276c634ded
William Sawyer <wmsawyer2609@gmail.com>
2024-07-30 07:45:51 +0000

    Extract config retrieval

--- status_check_runner/config.py
+++ status_check_runner/config.py
@@ -13,6 +13,11 @@ class Config(DefaultChecker.Config):
     typescript: Optional[TypeScriptChecker.Config] = None
 
 
+def get_config() -> Config:
+    with open("status-checks.json", "r", encoding="utf-8") as file:
+        return Config.model_validate(json.loads(file.read()))
+
+
 def update_config_json_schema():
     with open("config-schema.json", "w", encoding="utf-8") as file:
         json.dump(Config.model_json_schema(), file, indent=4)
